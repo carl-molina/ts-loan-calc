b2b84014429cde489ea8ccab05d5b367f770f64e
Carl Molina <carlandrew.molina@gmail.com>
2024-02-24 18:28:37 -0800

    Initial playground branch for testing

--- calculator.ts
+++ calculator.ts
@@ -1,15 +1,12 @@
 "use strict";
-
-import { IForm, IResult } from "./interface.js";
-
 // Find DOM element once rather than finding each time on use.
-const calcForm: HTMLFormElement = document.querySelector("#calc-form")!;
-const amountInput: HTMLInputElement = document.querySelector("#loan-amount")!;
-const yearsInput: HTMLInputElement = document.querySelector("#loan-years")!;
-const rateInput: HTMLInputElement = document.querySelector("#loan-rate")!;
-const resultArea: HTMLSpanElement = document.querySelector("#calc-monthly-payment")!;
+const calcForm = document.getElementById("calc-form");
+const amountInput = document.getElementById("loan-amount");
+const yearsInput = document.getElementById("loan-years");
+const rateInput = document.getElementById("loan-rate");
+const resultArea = document.getElementById("calc-monthly-payment");
 
-const resultHistory: IResult[] = [];
+const resultHistory = [];
 
 /** Retrieve form values.
  *
@@ -17,7 +14,7 @@ const resultHistory: IResult[] = [];
  *
  * */
 
-function getFormValues(): IForm {
+function getFormValues() {
   return {
     amount: Number(amountInput.value),
     years: Number(yearsInput.value),
@@ -27,7 +24,7 @@ function getFormValues(): IForm {
 
 /** Calculate monthly payment and return. */
 
-function calcMonthlyPayment({ amount, years, rate }: IForm ): number {
+function calcMonthlyPayment({ amount, years, rate }) {
   const monthsInYear = 12;
   const monthlyRate = (rate / 100) / monthsInYear;
   const n = Math.floor(years * monthsInYear);
@@ -39,16 +36,16 @@ function calcMonthlyPayment({ amount, years, rate }: IForm ): number {
 
 /** Get form values, calculate, format to 2 decimal places, and display. */
 
-function getFormValuesAndDisplayResults(): void {
+function getFormValuesAndDisplayResults() {
   const { amount, years, rate } = getFormValues();
-  const payment: number = calcMonthlyPayment({ amount, years, rate });
+  const payment = calcMonthlyPayment({ amount, years, rate });
   resultHistory.push({ amount, years, rate, payment });
   resultArea.innerText = "$" + payment.toFixed(2);
 }
 
 /** Set initial form values and show initial results. Called at app start. */
 
-function setInitialValues(): void {
+function setInitialValues() {
   amountInput.value = "10000";
   yearsInput.value = "10";
   rateInput.value = "4.5";
@@ -57,13 +54,11 @@ function setInitialValues(): void {
 
 /** Start: set form defaults & display; attach form submit event listener. */
 
-function start(): void {
+function start() {
   setInitialValues();
 
-  calcForm.addEventListener("submit", function (evt: SubmitEvent) {
+  calcForm.addEventListener("submit", function (evt) {
     evt.preventDefault();
     getFormValuesAndDisplayResults();
   });
 }
-
-start();
